1f24557de993b0013987c9a32aa77e2a96976088
vishu <vishwajeetsinghchauhan63@gmail.com>
2023-12-31 10:29:35 +0530

    image added in header

--- My-portfolio/src/container/Header/Header.jsx
+++ My-portfolio/src/container/Header/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => (
       <img src={images.profile} alt="profile_bg" />
       <motion.img
         whileInView={{ scale: [0, 1] }}
-        transition={{ duration: 1, ease: 'easeInOut' }}
+        transition={{ duration: 1, ease: "easeInOut" }}
         src={images.circle}
         alt="profile_circle"
         className="overlay_circle"
@@ -57,11 +57,13 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.react, images.javascript, images.git,].map((circle, index) => (
-        <div className="circle-cmp app__flex" key={`circle-${index}`}>
-          <img src={circle} alt="profile_bg" />
-        </div>
-      ))}
+      {[images.react, images.javascript, images.node, images.git].map(
+        (circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="profile_bg" />
+          </div>
+        )
+      )}
     </motion.div>
   </div>
 );
